5b6a1ea15cee5c85fd849d087c46a00b37d3f00a
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-09-09 14:26:13 +0530

    Create NAND switch_tb.v

---
+++ Assignment1/NANDusingswitchandgate/NAND switch_tb.v
@@ -0,0 +1,17 @@
+module nand_switch_tb;
+  reg a,b;
+  wire f;
+  nand_switch uut(.a(a),.b(b),.f(f));
+  intial begin
+    $dumpfile("nand_switch_tb.vcd");
+    $dumpvars;
+    $display("  TIME  | A  B |  OUT  ");
+    $display("-----------------------");
+    $monitor("  %4t   | %b %b |  %b  ",$time,a,b,f);
+    a=0; b=0; #10;
+    a=0; b=1; #10;
+    a=1; b=0; #10;
+    a=1; b=1; #10;
+    $finish;
+  end
+  endmmodule
