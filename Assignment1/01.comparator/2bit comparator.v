8d3d12906a4be64441e240d560bc7d4b22eddb38
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-09-09 15:32:40 +0530

    Update 2bit comparator.v

--- Assignment1/01.comparator/2bit comparator.v
+++ Assignment1/01.comparator/2bit comparator.v
@@ -1,37 +1,35 @@
-module comparator_tb;
-  reg  [1:0] a, b;
-  wire Eq, Less, Greater;
+module comparator(
+  output Eq, Less, Greater,
+  input [1:0] a,b);
+  wire x0, x1, x2, x3;
+  wire w0, w1;
+  wire nb0, nb1;
+  wire na0, na1;
+  wire l1,l0,g1,g0;
 
-  comparator uut (
-  .a(a),  
-  .b(b), 
-  .Eq(Eq), 
-  .Less(Less), 
-  .Greater(Greater));
+   // inverters
+  not(na0, a[0]);
+  not(na1, a[1]);
+  not(nb0, b[0]);
+  not(nb1, b[1]);
 
-  initial begin
-    $dumpfile("comp_wave.vcd");
-    $dumpvars(0, comparator_tb);
-    $display("Time | A   B | Equal Lesser Greater");
-    $display("-----------------------------------");
-    $monitor("%4t | %b %b |  %b      %b        %b", $time, a, b, Eq, Less, Greater);
+    //Equality output
+  and(x0, na0, nb0);
+  and(x1, a[0], b[0]);
+  and(x2, na1, nb1);
+  and(x3, a[1], b[1]);
+  or(w0, x0, x1);
+  or(w1, x2, x3);
+  and(Eq, w0, w1);
 
-    a=2'b00; b=2'b00; #10;
-    a=2'b00; b=2'b01; #10;
-    a=2'b00; b=2'b10; #10;
-    a=2'b00; b=2'b11; #10;
-    a=2'b01; b=2'b00; #10;
-    a=2'b01; b=2'b01; #10;                                                                            
-    a=2'b01; b=2'b10; #10;
-    a=2'b01; b=2'b11; #10;                                                                            
-    a=2'b10; b=2'b00; #10; 
-    a=2'b10; b=2'b01; #10;
-    a=2'b10; b=2'b10; #10;
-    a=2'b10; b=2'b11; #10;
-    a=2'b11; b=2'b00; #10;                                                                            
-    a=2'b11; b=2'b01; #10;
-    a=2'b11; b=2'b10; #10;
-    a=2'b11; b=2'b11; #10;
-    $finish;
-  end
+    //Lesser output
+  and(l1, na1, b[1]);
+  and(l0, w1, na0, b[0]);
+  or(Less, l1, l0);
+
+    //Greater output
+  and(g1, nb1, a[1]);
+  and(g0, w1, nb0, a[0]);
+  or(Greater, g1, g0);
+
 endmodule
