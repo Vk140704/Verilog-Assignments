67321021615e710313c2b17016a86c5561539dc8
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-09-09 23:01:54 +0530

    Create concat.v

---
+++ Assignment_3/Dataflow Modeling/08.qn/concat.v
@@ -0,0 +1,9 @@
+/*8. Create a module using concatenation operator:
+assign y = {a, b[0], c[1]};*/
+
+module concat(a,b,c,y);
+input a;
+input [1:0]b,c;
+output [2:0]y;
+assign y={a,b[0],c[1]};
+endmodule
