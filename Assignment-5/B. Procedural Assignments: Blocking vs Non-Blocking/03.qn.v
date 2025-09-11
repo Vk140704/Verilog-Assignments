e7f23806691b2986fc8f64133ff77a5371d87d80
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-09-11 21:08:00 +0530

    Create 03.qn.v

---
+++ Assignment-5/B. Procedural Assignments: Blocking vs Non-Blocking/03.qn.v
@@ -0,0 +1,16 @@
+//11. Explain how = and <= affect simulation order with examples.
+module block_vs_nonblock;
+reg a,b,c,d;
+initial begin
+a=0;b=0;
+#10 a=1;#5 b=1;
+end
+initial begin
+c<=0;d<=0;
+#5 c<=1; d<=c;
+end 
+initial begin
+$monitor("time=%t  a=%b  b=%b  c=%b  d=%b",$time,a,b,c,d);
+#100 $finish;
+end
+endmodule
