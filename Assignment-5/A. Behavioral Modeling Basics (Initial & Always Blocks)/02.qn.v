da218e30bf7086a91a7fac050c4d3923ac45ad37
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-09-11 15:55:06 +0530

    Update 02.qn.v

--- Assignment-5/A. Behavioral Modeling Basics (Initial & Always Blocks)/02.qn.v
+++ Assignment-5/A. Behavioral Modeling Basics (Initial & Always Blocks)/02.qn.v
@@ -1,3 +1,13 @@
 /* Simulate a 16-bit register being initialized and incremented inside an always block.*/
 Answer
-module 
+module reg_increment;
+  reg [15:0] a;
+
+  initial begin
+  $monitor("time=%t   a=%b ",$time,a);
+    a= 16'h0000;
+  #100 $finish;
+  end
+
+  always #10 a= a + 1; // Increment every 10 time units
+endmodule
