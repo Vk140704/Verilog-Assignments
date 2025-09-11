732eb422cc9a31c182a8d6b6cf7c0b23cff98cda
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-09-11 22:14:51 +0530

    Update ALU.v

--- Assignment1/02.2bit ALU_struct/ALU.v
+++ Assignment1/02.2bit ALU_struct/ALU.v
@@ -1,22 +1,23 @@
-module alu(input [3:0] a, input [3:0] b, input [2:0] op, output reg [3:0] result);
-  always @(*)
-    case (op)
-      3'b000: result = a + b;
-      3'b001: result = a - b;
-      3'b010: result = a & b;
-      3'b011: result = a | b;
-      3'b100: result = a ^ b;
-      3'b101: result = ~a;
-      3'b110: result = a << 1;
-      3'b111: result = a >> 1;
-    endcase
-endmodule
-
-module multiplier(input [3:0] a, input [3:0] b, output [7:0] product);
-  assign product = a * b;
-endmodule
-
-module divider(input [3:0] a, input [3:0] b, output [3:0] quotient, output [3:0] remainder);
-  assign quotient = a / b;
-  assign remainder = a % b;
-endmodule
+  module alu(input [1:0] a, input [1:0] b, input [1:0] op, output reg [3:0] result);
+    always @(*)
+      case (op)
+        2'b00: result = a + b;
+        2'b01: result = a - b;
+        2'b10: result = a & b;
+        2'b11: result = a | b;
+      endcase
+  endmodule
+  
+        module multiplier(input [1:0] a, input [1:0] b, output [3:0] product);
+    assign product = a * b;
+  endmodule
+  
+  module divider(input [1:0] a, input [1:0] b, output [3:0] quotient, output [1:0] remainder);
+    always @(*)begin
+      if (b == 0) begin
+      quotient = 4'b0000; 
+      remainder = a;        
+    end else begin
+    assign quotient = a / b;
+    assign remainder = a % b;
+  endmodule
