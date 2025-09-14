ffc95a455134936dbc0cc9cd28dba7910efe42c6
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-09-14 21:23:53 +0530

    Update 01.qn.v

--- Assignment-5/C. Race Conditions & Scheduling/01.qn.v
+++ Assignment-5/C. Race Conditions & Scheduling/01.qn.v
@@ -1,1 +1,17 @@
 //19. Demonstrate a race condition using two always blocks assigning to the same variable.
+
+module race;
+reg a,clk;
+always @(posedge clk)
+  a=0;
+always @(posedge clk)
+  a=1;
+initial begin 
+	clk=0;
+forever #5 clk=~clk;
+end
+initial begin
+$monitor("Time=%t  |  clk=%b  |  a=%b  |",$time,clk,a);
+#100 $finish;
+end 
+endmodule 
