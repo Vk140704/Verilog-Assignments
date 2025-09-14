424e0c7454fef89b8e888f755828256f00b69ac8
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-09-14 23:55:05 +0530

    Update 01.qn.v

--- Assignment-5/E. Conditional Statements/01.qn.v
+++ Assignment-5/E. Conditional Statements/01.qn.v
@@ -1,17 +1,10 @@
 //37. Implement a 2:1 multiplexer using if statement.
-module mux(input i0.i1,i2,i3,sel,output y);
-  wire w1,w2;
-  
+module mux(input i0,i1,sel,output y);
   always@begin
     if(sel==0)
-      w1=i0;
+      y=0;
     else
-      w1=i2;
+      y=1;
   end 
-  always @begin
-    if (sel==0)
-      w2=i3;
-    else
-      w2=i4;
-  end
+  
       
