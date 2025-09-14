40556576c2b76b8740a6117ce9b37f648ea6b8ed
Vishalpalanisamy <vishal.nkl04@gmail.com>
2025-09-14 22:36:14 +0530

    Create qn.06.v

---
+++ Assignment-5/C. Race Conditions & Scheduling/qn.06.v
@@ -0,0 +1,23 @@
+//24. Simulate two flip-flops exchanging values with blocking and non-blocking assignments.
+
+module two_ff_nonblocking;
+  reg clk;
+  reg q1, q2;
+
+  always @(posedge clk) begin
+    q1 <= q2;
+    q2 <= q1;
+  end
+
+  initial begin
+    clk = 0;
+    q1 = 0; q2 = 1;
+    $display("Time | clk | q1 q2");
+    $monitor("%4t |  %b  |  %b  %b", $time, clk, q1, q2);
+
+    repeat(4) begin
+      #5 clk = ~clk;
+    end
+    #5 $finish;
+  end
+endmodule
